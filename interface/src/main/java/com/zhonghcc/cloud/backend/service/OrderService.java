40a7b462950b1e85c646235a42c1443bc4cc7680
zhonghcc <zhonghcc@gmail.com>
2019-08-29 20:44:15 +0800

    rpc

--- interface/src/main/java/com/zhonghcc/cloud/backend/service/OrderService.java
+++ interface/src/main/java/com/zhonghcc/cloud/backend/service/OrderService.java
@@ -2,6 +2,7 @@ package com.zhonghcc.cloud.backend.service;
 
 import com.zhonghcc.cloud.backend.model.Order;
 import com.zhonghcc.cloud.common.model.ResponseVO;
+import com.zhonghcc.cloud.common.rpc.FeignProtostuffConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +12,10 @@ import java.util.List;
 /**
  * Created by Chenzhi on 2019/8/29.
  */
-@FeignClient("backend")
+@FeignClient(name="backend",configuration = FeignProtostuffConfiguration.class)
 public interface OrderService {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getOrders")
+    @RequestMapping(method = RequestMethod.GET, value = "/getOrders",consumes = "application/x-protobuf", produces = "application/x-protobuf")
     List<Order> getOrders();
 
     @RequestMapping(method = RequestMethod.POST, value = "/update")
