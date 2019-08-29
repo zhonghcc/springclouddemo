91b429012bd1fe4d61426125f2aa5b140af5cb1c
zhonghcc <zhonghcc@gmail.com>
2019-08-30 01:19:14 +0800

    test

---
+++ surface/src/test/java/com/zhonghcc/cloud/surface/controller/OrderControllerTest.java
@@ -0,0 +1,32 @@
+package com.zhonghcc.cloud.surface.controller;
+
+import com.zhonghcc.cloud.backend.model.Order;
+import com.zhonghcc.cloud.backend.service.OrderService;
+import com.zhonghcc.cloud.common.model.ResponsePageVO;
+import com.zhonghcc.cloud.serface.SerfaceApplicationTests;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+
+public class OrderControllerTest extends SerfaceApplicationTests {
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    public void getList() {
+        ResponsePageVO<Order> responsePageVO = orderService.getOrders();
+
+        long start = System.currentTimeMillis();
+        for(int i=0;i<100000;i++){
+            orderService.getOrders();
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("total cost="+(end-start));
+        System.out.println(responsePageVO);
+
+
+    }
+}
